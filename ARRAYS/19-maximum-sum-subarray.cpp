7e5062a8c0999340dca962e31a72171f417bfa18
SHRUTI SINGH <shruticourses16@gmail.com>
2022-08-14 09:49:13 +0530

    Update 19-maximum-sum-subarray.cpp

--- ARRAYS/19-maximum-sum-subarray.cpp
+++ ARRAYS/19-maximum-sum-subarray.cpp
@@ -2,9 +2,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// //TC=O(N^2) SC=O(1)
+// void maxsum(int arr[],int n){
+//     int largest = INT_MIN;
+//     for(int i=0;i<n;++i){
+//         int count = 0;
+//         for(int j=i;j<n;++j){
+//             count = count + arr[j];
+//             largest = max(count,largest);
+//         }
+//     }
+//     cout<<largest;
+// }
 
-int maxsum(int arr[],int n){
-    
+//TC=O(N)  SC=O(1)
+void maxsum(int arr[],int n){
+    int largest = arr[0];
+    int sum = arr[0];
+    for(int i=1;i<n;++i){
+        sum = max(sum+arr[i],arr[i]);
+        largest = max(largest,sum);
+    }
+    cout<<largest;
 }
 
 int main(){
@@ -13,8 +32,8 @@ int main(){
     int arr[n];
     for(int i=0;i<n;++i){
         cin>>arr[i];
-    } 
-    cout<<maxsum(arr,n);
+    }
+    maxsum(arr,n);
 
 }
 
