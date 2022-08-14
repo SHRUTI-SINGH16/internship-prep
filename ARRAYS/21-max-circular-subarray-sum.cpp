8c3f532f5110b4dede718b1f185de9817f9e9c1c
SHRUTI SINGH <shruticourses16@gmail.com>
2022-08-14 18:31:48 +0530

    Update 21-max-circular-subarray-sum.cpp

--- ARRAYS/21-max-circular-subarray-sum.cpp
+++ ARRAYS/21-max-circular-subarray-sum.cpp
@@ -2,8 +2,40 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void longsub(int arr[],int n){
+
+// //tc=o(n^2) sc=o(n)
+// void maxcircular(int arr[],int n){
+//     int largest = 0;
+//     int indexsum;
+//     int indexmaxsum;
+//     for(int i=0;i<n;++i){
+//         indexsum = arr[i];
+//         indexmaxsum = arr[i];
+//         for(int j=1;j<n;++j){
+//             int index = (i+j)%n;
+//             indexsum = indexsum + arr[index];
+//             indexmaxsum = max(indexmaxsum,indexsum);
+//         }
+//         largest = max(largest,indexmaxsum);
+//     }
     
+//     cout<<largest;
+// }
+
+
+//tc=O(N)  SC=O(1)
+//USED MODIFIED KANDANES ALGO
+//TOTAL-MIN SUM SUBARRAY
+void maxcircular(int arr[],int n){
+    int largest = INT_MAX;
+    int sum = arr[0];
+    int total = arr[0];
+    for(int i=1;i<n;++i){
+        total = total + arr[i];
+        sum = min(sum+arr[i],arr[i]);
+        largest = min(sum,largest);
+    }
+    cout<<total-largest;
 }
 
 int main(){
