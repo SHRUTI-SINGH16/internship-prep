3ce346a562def05c0c8d4599c13144188a4aa2bd
SHRUTI SINGH <shruticourses16@gmail.com>
2022-08-22 12:55:48 +0530

    Update 27-N-bonacchi-number.cpp

---
+++ ARRAYS/27-N-bonacchi-number.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void nbonacchi(int n,int m){
+    int arr[m];
+    for(int i=0;i<n-1;++i){
+        arr[i] = 0;
+    }
+    arr[]
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int m;
+    cin>>m;
+    
+    nbonacchi(n,m);
+
+}
+
+
