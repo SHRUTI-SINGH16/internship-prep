d57d7663c9ffc095c46f428ec7fa5564fca6c9a8
SHRUTI SINGH <shruticourses16@gmail.com>
2022-09-04 18:32:27 +0530

    Create 01-bubble-sort.cpp

---
+++ 06-SORTING/01-bubble-sort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubblesort(int arr[],int n){
+    for(int i=0;i<n-1;++i){
+        for(int j=0;j<n-i-1;++j){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j+1],arr[j]);
+            }
+        }
+    }
+    for(int i=0;i<n;++i){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;++i){
+        cin>>arr[i];
+    }
+    bubblesort(arr,n); 
+}
+
+
