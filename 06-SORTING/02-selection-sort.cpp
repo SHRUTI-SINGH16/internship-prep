eeaf5d119885100838174c376ef8aefaa15b0809
SHRUTI SINGH <shruticourses16@gmail.com>
2022-09-05 19:11:32 +0530

    commit

--- 06-SORTING/02-selection-sort.cpp
+++ 06-SORTING/02-selection-sort.cpp
@@ -2,18 +2,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//TC=O(n^2)  sc=O(1)
-void bubblesort(int arr[],int n){
+//TC=O
+void selectionsort(int arr[],int n){
     for(int i=0;i<n-1;++i){
-        bool swapped = false;
         for(int j=0;j<n-i-1;++j){
             if(arr[j]>arr[j+1]){
                 swap(arr[j+1],arr[j]);
-                swapped = true;
             }
-        }
-        if(swapped==false){
-            break;
         }
     }
     for(int i=0;i<n;++i){
@@ -30,7 +25,7 @@ int main(){
     for(int i=0;i<n;++i){
         cin>>arr[i];
     }
-    bubblesort(arr,n); 
+    selectionsort(arr,n); 
 }
 
 
