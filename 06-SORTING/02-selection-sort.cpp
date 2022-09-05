965f2207dcac073c4eff0a01bee703adac3713a7
SHRUTI SINGH <shruticourses16@gmail.com>
2022-09-05 19:31:10 +0530

    Update 02-selection-sort.cpp

--- 06-SORTING/02-selection-sort.cpp
+++ 06-SORTING/02-selection-sort.cpp
@@ -5,11 +5,13 @@ using namespace std;
 //TC=O
 void selectionsort(int arr[],int n){
     for(int i=0;i<n-1;++i){
-        for(int j=0;j<n-i-1;++j){
-            if(arr[j]>arr[j+1]){
-                swap(arr[j+1],arr[j]);
+        int min_index = i;
+        for(int j=i+1;j<n;++j){
+            if(arr[j]<arr[min_index]){
+                min_index = j;
             }
         }
+        swap(arr[min_index],arr[i]);
     }
     for(int i=0;i<n;++i){
         cout<<arr[i]<<" ";
