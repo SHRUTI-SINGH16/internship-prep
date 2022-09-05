71e7c38394570ebe4d0f822a37daf93e6d1a3066
SHRUTI SINGH <shruticourses16@gmail.com>
2022-09-05 19:40:34 +0530

    commit

--- 06-SORTING/03-insertion-sort.cpp
+++ 06-SORTING/03-insertion-sort.cpp
@@ -3,16 +3,17 @@
 using namespace std;
 
 //TC=O(n^2) sc=o(1)
-void selectionsort(int arr[],int n){
-    for(int i=0;i<n-1;++i){
-        int min_index = i;
-        for(int j=i+1;j<n;++j){
-            if(arr[j]<arr[min_index]){
-                min_index = j;
-            }
+void insertionsort(int arr[],int n){
+    for(int i=1;i<n;++i){
+        int key = arr[i];
+        int j = i-1;
+        while(j>=0 && arr[j]>key){
+            arr[j+1] = arr[j];
+            j--;
         }
-        swap(arr[min_index],arr[i]);
+        arr[j+1] = key;
     }
+
     for(int i=0;i<n;++i){
         cout<<arr[i]<<" ";
     }
@@ -27,7 +28,7 @@ int main(){
     for(int i=0;i<n;++i){
         cin>>arr[i];
     }
-    selectionsort(arr,n); 
+    insertionsort(arr,n); 
 }
 
 
