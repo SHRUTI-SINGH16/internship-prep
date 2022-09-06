80597c4164880a53501801c6a100529f0fe8184d
SHRUTI SINGH <shruticourses16@gmail.com>
2022-09-06 12:15:51 +0530

    commit

--- 06-SORTING/05-merge-function-of-merge-sort.cpp
+++ 06-SORTING/05-merge-function-of-merge-sort.cpp
@@ -2,60 +2,64 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// //TC=O((n+m)log(n+m)) sc=o(n+m)
-// void mergearray(int arr[],int brr[],int n, int m){
-//     int ans[n+m];
-//     for(int i=0;i<n;++i){
-//         ans[i] = arr[i];
-//     }
-//     for(int i=0;i<m;++i){
-//         ans[n+i] = brr[i];
-//     }
-//     sort(ans,ans+m+n);
-//     for(int i=0;i<n+m;++i){
-//         cout<<ans[i]<<" ";
-//     }
-//     cout<<endl;
-// }
+//TC=O(n) sc=o(n)
+void mergefunction(int arr[],int n, int low, int mid, int high){
+    int nleft = mid+1-low;
+    int nright = high-mid;
+    int left[nleft];
+    int right[nright];
+    for(int i=0;i<nleft;++i){
+        left[i] = arr[low+i];
+    }
+    for(int i=0;i<nright;++i){
+        right[i] = arr[nleft+i];
+    }
 
-//TC=O(n+m) sc=o(1)
-void mergearray(int arr[],int brr[],int n, int m){
     int i=0;
     int j=0;
-    while(i<n && j<m){
-        if(arr[i]<=brr[j]){
-            cout<<arr[i]<<endl;
+    int k=0;
+    while(i<nleft && j<nright){
+        if(left[i]<=right[j]){
+            arr[k] = left[i];
             ++i;
+            ++k;
         }else{
-            cout<<brr[j]<<endl;
+            arr[k] = right[j];
             ++j;
+            ++k;
         }
     }
-    while(i<n){
-        cout<<arr[i]<<endl;
+    while(i<nleft){
+        arr[k] = left[i];
+        ++k;
         ++i;
     }
-    while(j<m){
-        cout<<brr[j]<<endl;
+    while(j<nright){
+        arr[k] = right[j];
+        ++k;
         ++j;
     }
+
+    for(int i=0;i<n;++i){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
     
     
 }
 
 
 int main(){
-    int n,m;
-    cin>>n>>m;
+    int n;
+    cin>>n;
+    int low,mid,high;
+    cin>>low>>mid>>high;
     int arr[n];
-    int brr[m];
     for(int i=0;i<n;++i){
         cin>>arr[i];
     }
-    for(int i=0;i<m;++i){
-        cin>>brr[i];
-    }
-    mergearray(arr,brr,n,m); 
+    
+    mergefunction(arr,n,low,mid,high); 
     
 }
 
