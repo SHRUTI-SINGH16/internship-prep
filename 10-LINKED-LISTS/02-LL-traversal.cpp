96630cbe2b08f53764d69a79d99f6aaddd7b7f97
SHRUTI SINGH <shruticourses16@gmail.com>
2022-10-01 17:00:51 +0530

    commit

---
+++ 10-LINKED-LISTS/02-LL-traversal.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+    Node(int x){
+        data=x;
+        next=NULL;
+    }
+};
+
+void traversal(Node* head){
+    Node* curr = head;
+    while(curr!= NULL){
+        cout<<curr->data<<endl;
+        curr=curr->next;
+    }
+}
+
+int main(){
+    Node* head;
+    Node* temp = new Node(10);
+    temp->next = new Node(20);
+    temp->next->next = new Node(30);
+    temp->next->next->next = new Node(40);
+    head = temp;
+    traversal(head);
+    return 0;
+}
