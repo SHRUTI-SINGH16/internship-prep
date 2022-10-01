f119ff2e087ab313048a0d83f189434901c43d3f
SHRUTI SINGH <shruticourses16@gmail.com>
2022-10-01 18:14:21 +0530

    commit

---
+++ 10-LINKED-LISTS/05-insert-at-end.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+    Node(int x){
+        data=x;
+        next=NULL;
+    }
+};
+
+void insertEnd(Node* &head,int x){
+    Node* curr = new Node(x);
+    Node* temp = head;
+
+    if(head==NULL){
+        head = curr;
+        return;
+    }
+    while(temp->next!=NULL){
+        temp = temp->next;
+    }
+    temp->next = curr;
+}
+
+void traversal(Node* head){
+    Node* curr = head;
+    while(curr!= NULL){
+        cout<<curr->data<<endl;
+        curr=curr->next;
+    }
+}
+
+int main(){
+    int x;
+    cin>>x;
+    Node* head = NULL;
+    insertEnd(head,10);
+    insertEnd(head,20);
+    insertEnd(head,30);
+    insertEnd(head,x);
+    traversal(head);
+    return 0;
+}
