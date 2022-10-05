1791e0a6a4c19c146ee2d0652a875f296b416fbc
SHRUTI SINGH <shruticourses16@gmail.com>
2022-10-05 12:24:30 +0530

    COMMIT

---
+++ 10-LINKED-LISTS/12-insert-at-begin-in-DLL.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+    Node* prev;
+    Node(int x){
+        data = x;
+        prev = NULL;
+        next = NULL;
+    }
+};
+
+void insertBegin(Node* &head,int x){
+    Node* temp = head;
+    Node* newNode = new Node(x);
+    newNode->next = temp;
+    head = newNode; 
+}
+
+void traversal(Node* &head){
+    Node* curr = head;
+    while(curr!= NULL){
+        cout<<curr->data<<" ";
+        curr=curr->next;
+    }
+    cout<<endl;
+}
+
+int main(){
+    Node* head = NULL;
+    insertBegin(head,10);
+    insertBegin(head,20);
+    insertBegin(head,30);
+    traversal(head);
+    return 0;
+}
